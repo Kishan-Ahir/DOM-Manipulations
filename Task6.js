d4c05de9b8d132cb12c696b4255f600f5ec2088f
Kishan-Ahir <chandravadiyakishan77@gmail.com>
2023-02-01 14:01:14 +0530

    task 6 js

---
+++ Task6.js
@@ -0,0 +1,22 @@
+// QUERRY SELECTOR
+
+//let item = document.querySelector(".list-group-item");
+//item.style.color = "red";
+
+//item2 = document.querySelector(".list-group-item:nth-child(2)");
+//item2.style.color = "green";
+
+//let item3 = document.querySelector(".list-group-item:nth-child(3)")
+//item3.style.display = "none";
+
+//  QUERRY SELECTOR  ALL  
+
+//let items = document.querySelectorAll(".list-group-item");
+//items[1].style.color = "green";
+
+//let odd=document.querySelectorAll(".list-group-item:nth-child(odd)");
+
+//for(let i=0;i<odd.length;i++)
+//{
+    //odd[i].style.backgroundColor = "green";
+//}
