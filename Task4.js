0b7aadd603faa56da3181624d248f582e64ee1c2
Kishan-Ahir <chandravadiyakishan77@gmail.com>
2023-02-01 13:50:25 +0530

    task 4 js

---
+++ Task4.js
@@ -0,0 +1,14 @@
+//GET ELEMENTS BY CLASS NAME:
+//let items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+
+//items[1].style.backgroundColor = "lightpink";
+
+//Will give an error
+//items.style.backgroundColor = "red";
+
+//for(let i=0;i<items.length;i++)
+//{
+    //items[i].style.backgroundColor = "lightyellow";
+//};
+
